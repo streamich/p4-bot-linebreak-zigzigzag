5115cd2d4890ac73080d91e52298202a60fc151b
vad <wanyxspe@gmail.com>
2017-03-26 17:52:15 +0100

    no-deps

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 "use strict";
-var P4CompLinebreakZigzigzag = require('p4-comp-linebreak-zigzigzag').P4CompLinebreakZigzigzag;
+exports.P4CompLinebreakZigzigzag = function P4CompLinebreakZigzigzag() {
+    return function () { return ['div', { style: { width: 100, margin: '40px auto' } },
+        ['svg', { width: 100, height: 40 },
+            ['path', {
+                    d: 'M0 32 L35 20 L20 32 L55 20 L40 32 L75 20 L60 32 L95 20',
+                    style: {
+                        fill: 'none',
+                        stroke: '#aaa',
+                        strokeWidth: '5px',
+                    }
+                }]
+        ]
+    ]; };
+};
 exports.bot = function (_a) {
     var view = _a.view, manifest = _a.manifest;
     manifest({
         name: 'Zigzigzag Linebreak Bot',
-        version: '0.0.1',
+        version: '0.0.2',
         icon: {
             '48': 'http://192.168.1.10:8888/img/zigzigzag-bot-48x48.png',
         },
     });
-    view('hr', P4CompLinebreakZigzigzag);
+    view('hr', exports.P4CompLinebreakZigzigzag);
 };
--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
-const {P4CompLinebreakZigzigzag} = require('p4-comp-linebreak-zigzigzag');
+import {TComp} from 'p4-comp';
+
+
+export const P4CompLinebreakZigzigzag: TComp<{}, {}> = function P4CompLinebreakZigzigzag() {
+    return () => ['div', {style: {width:100, margin:'40px auto'}},
+        ['svg', {width: 100, height: 40},
+            ['path', {
+                d: 'M0 32 L35 20 L20 32 L55 20 L40 32 L75 20 L60 32 L95 20',
+                style: {
+                    fill: 'none',
+                    stroke: '#aaa',
+                    strokeWidth: '5px',
+                }
+            }]
+        ]
+    ];
+};
 
 
 export const bot = ({view, manifest}) => {
 
     manifest({
         name: 'Zigzigzag Linebreak Bot',
-        version: '0.0.1',
+        version: '0.0.2',
         icon: {
             '48': 'http://192.168.1.10:8888/img/zigzigzag-bot-48x48.png',
         },
